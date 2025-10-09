74a1ec05bd3db2456ce3bfd276034b6d845813cb
Louis Roux <lroux020805@gmail.com>
2025-10-09 12:20:16 +0200

    maj stat

---
+++ src/dao/statistique_dao.py
@@ -0,0 +1,8 @@
+import logging
+
+from utils.singleton import Singleton
+from utils.log_decorator import log
+
+from dao.db_connection import DBConnection
+
+from business_object.statistique import Statistique
