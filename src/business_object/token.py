4e5868f7c82af684e8cc80ec5f47e1dd706ac86e
maytena.labinsky <maytena.labinsky@gmail.com>
2025-10-21 11:28:40 +0200

    token metier ok ok

--- src/business_object/token.py
+++ src/business_object/token.py
@@ -1,5 +1,5 @@
 from datetime import date
-from src.business_object.utilisateur import Utilisateur
+from src.business_object.utilisateur import Utilisateur as u
 
 
 class Token:
@@ -16,10 +16,10 @@ class Token:
         date d'expiration du jeton (privé)
     """
 
-    def __init__(self, jeton: str, utilisateur: Utilisateur, date_expiration: date):
+    def __init__(self, jeton: str, utilisateur: u, date_expiration : date ):
         """Constructeur"""
         self.__jeton = jeton
-        self.__id_user = utilisateur.id_user
+        self.__id_user = u.id_user
         self.__date_expiration = date_expiration
 
     @property
@@ -29,9 +29,7 @@ class Token:
     @jeton.setter
     def jeton(self, value):
         if value is not None and not isinstance(value, str):
-            raise ValueError(
-                "Le jeton d'authentification doit être une chaîne de caractères"
-            )
+            raise ValueError("Le jeton d'authentification doit être une chaîne de caractères")
         self.__jeton = value
 
     @property
@@ -47,10 +45,8 @@ class Token:
     def __eq__(self, other):
         if not isinstance(other, Token):
             return False
-        return (
-            self.__id_user == other.__id_user and self.jeton == other.__jeton
-        )  # ici je ne sais
-        # pas si on doit comparer les deux ou que un seul des deux et si oui lequel
+        return self.__id_user == other.__id_user and self.jeton == other.__jeton #ici je ne sais
+        #pas si on doit comparer les deux ou que un seul des deux et si oui lequel
 
     def __repr__(self):
         return f"Token(user_id={self.__id_user}, jeton='{self.__jeton}',expiration={self.__date_expiration})"
