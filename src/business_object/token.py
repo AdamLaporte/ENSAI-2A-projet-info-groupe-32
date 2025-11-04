f5d2d32b1697ba6a21ca31afb544b9171b866a53
maytena <maytena.labinsky@eleve.ensai.fr>
2025-11-04 09:16:31 +0000

    petit changement

--- src/business_object/token.py
+++ src/business_object/token.py
@@ -8,45 +8,45 @@ class Token:
 
     Attributs
     ----------
-    __jeton : str
+    _jeton : str
         jeton d'authentification (privé)
-    __id_user : str
+    _id_user : int
         identifiant du joueur lié à ce jeton (privé).
-    __date_expiration :
+    _date_expiration :
         date d'expiration du jeton (privé)
     """
 
-    def __init__(self, jeton: str, u : Utilisateur, date_expiration : datetime ):
+    def __init__(self, jeton: str, u : Utilisateur, date_expiration : datetime ): #comment j'importe u 
         """Constructeur"""
-        self.__jeton = jeton
-        self.__id_user = u.id_user
-        self.__date_expiration = date_expiration
+        self._jeton = jeton
+        self._id_user = u.id_user
+        self._date_expiration = date_expiration
 
     @property
     def jeton(self):
-        return self.__jeton
+        return self._jeton
 
     @jeton.setter
     def jeton(self, value):
         if value is not None and not isinstance(value, str):
             raise ValueError("Le jeton d'authentification doit être une chaîne de caractères")
-        self.__jeton = value
+        self._jeton = value
 
     @property
     def date_expiration(self):
-        return self.__date_expiration
+        return self._date_expiration
 
     @date_expiration.setter
     def date_expiration(self, value):
         if value is not None and not isinstance(value, datetime):
             raise ValueError("La date d'expiration doit être une date")
-        self.__date_expiration = value
+        self._date_expiration = value
 
     def __eq__(self, other):
         if not isinstance(other, Token):
             return False
-        return self.__id_user == other.__id_user and self.jeton == other.__jeton #ici je ne sais
+        return self._id_user == other._id_user and self.jeton == other._jeton #ici je ne sais
         #pas si on doit comparer les deux ou que un seul des deux et si oui lequel
 
     def __repr__(self):
-        return f"Token(user_id={self.__id_user}, jeton='{self.__jeton}',expiration={self.__date_expiration})"
+        return f"Token(user_id={self._id_user}, jeton='{self._jeton}',expiration={self._date_expiration})"
